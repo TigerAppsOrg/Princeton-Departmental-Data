88edbd1077e6993d810c4ec8e4e12dc0cd82ee4a
joshuamotoaki <dachshunduniverse@gmail.com>
2024-01-12 10:57:51 -0800

    Add warning messages

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -14,17 +14,17 @@ const preprocess = () => {
                 return;
             }
             files.forEach(file => {
-                console.log(`Processing ${directory}/${file}:`);
+                const filename = directory + "/" + file;
                 const data = yaml.load(fs.readFileSync(`${directory}/${file}`, 'utf8'));
                 if (!data.hasOwnProperty('req_list')) return;
                 data.req_list = processReq(data.req_list);
-                data.req_list = reorder(data.req_list);
+                data.req_list = reorder(data.req_list, filename);
 
                 // Write a new file with the processed data in YAML
-                const processedDataYaml = yaml.dump(data);
-                fs.writeFileSync(`${directory}/${file}`, processedDataYaml, 'utf8');
-                const processedDataJson = JSON.stringify(data);
-                fs.writeFileSync(`json/${directory}/${file.split('.')[0]}.json`, processedDataJson, 'utf8');
+                // const processedDataYaml = yaml.dump(data);
+                // fs.writeFileSync(`${directory}/${file}`, processedDataYaml, 'utf8');
+                // const processedDataJson = JSON.stringify(data);
+                // fs.writeFileSync(`json/${directory}/${file.split('.')[0]}.json`, processedDataJson, 'utf8');
             })
         })
     })
@@ -40,13 +40,13 @@ const processReq = (req) => {
     return req;
 }
 
-const reorder = (req) => {
+const reorder = (req, filename) => {
     const FIELDS = ['name', 'min_needed', 'max_counted', 'no_crosslist', 'double_counting_allowed', 'max_common_with_major', 'pdfs_allowed', 'completed_by_semester', 'description', 'explanation', 'req_list', 'course_list', 'excluded_course_list', 'iw_relationship', 'no_req'];
     req.forEach((r) => {
         FIELDS.forEach((f) => {
             if (r.hasOwnProperty(f)) {
-                if (f === 'iw_relationship' || f === 'min_needed' || f === 'double_counting_allowed' || f === 'explanation' || f === 'description' || f === 'req_list' || f === 'course_list' || f === 'excluded_course_list' && r[f] === null) {
-                    console.log(f + " is null" + r.name)
+                if ((f === 'iw_relationship' || f === 'min_needed' || f === 'double_counting_allowed' || f === 'explanation' || f === 'description' || f === 'req_list' || f === 'course_list' || f === 'excluded_course_list') && r[f] === null) {
+                    console.log("WARNING -- " + filename + ": " + f + " is null in req: " + r.name);
                 }
                 const value = r[f];
                 delete r[f];
@@ -54,7 +54,7 @@ const reorder = (req) => {
             }
         });
         if (r.hasOwnProperty('req_list')) 
-            r.req_list = reorder(r.req_list);
+            r.req_list = reorder(r.req_list, filename);
     });
     return req;
 }
