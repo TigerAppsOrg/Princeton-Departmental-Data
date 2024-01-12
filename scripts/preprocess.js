c07097c1a81b9a3439f434c1e4c1af4ef91631f2
joshuamotoaki <dachshunduniverse@gmail.com>
2024-01-12 11:16:01 -0800

    Enhance script

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -1,11 +1,17 @@
+/**
+ * Preprocess the YAML files in the minors and certs directories.
+ * ! WARNING: Do not run this script unless you know what you are doing !
+ */
 const yaml = require('js-yaml');
 const fs = require('fs');
 
 /**
  * Preprocess each yaml file in each directory, adding max_counted: 1
- * when it is not present or null.
+ * when it is not present or null and reordering the fields to match
+ * the order in the schema.
+ * @param {boolean} write - whether to write the processed data to files
  */
-const preprocess = () => {
+const preprocess = (write) => {
     const DIRECTORIES = ["minors", "certs"];
     DIRECTORIES.forEach(directory => {
         fs.readdir(directory, (err, files) => {
@@ -15,38 +21,79 @@ const preprocess = () => {
             }
             files.forEach(file => {
                 const filename = directory + "/" + file;
-                const data = yaml.load(fs.readFileSync(`${directory}/${file}`, 'utf8'));
-                if (!data.hasOwnProperty('req_list')) return;
-                data.req_list = processReq(data.req_list);
-                data.req_list = reorder(data.req_list, filename);
+                const data = yaml.load(fs.readFileSync(`${directory}/${file}`,
+                    'utf8'));
 
-                // Write a new file with the processed data in YAML
-                // const processedDataYaml = yaml.dump(data);
-                // fs.writeFileSync(`${directory}/${file}`, processedDataYaml, 'utf8');
-                // const processedDataJson = JSON.stringify(data);
-                // fs.writeFileSync(`json/${directory}/${file.split('.')[0]}.json`, processedDataJson, 'utf8');
+                // Check description
+                if (!data.hasOwnProperty('description') || data.description === null) {
+                    console.log("NULL WARNING -- " + filename + ": description is null");
+                } else if (data.description.includes('\n\n')) {
+                    console.log("STYLE WARNING -- " + filename 
+                    + ": description has consecutive newlines");
+                }
+                
+                if (!data.hasOwnProperty('req_list')) {
+                    console.log("NULL WARNING -- " + filename + ": req_list is null... skipping");
+                    return;
+                }
+
+                if (!write) return;
+
+                // Write both yaml and json files
+                data.req_list = processReq(data.req_list, filename);
+                data.req_list = reorder(data.req_list, filename);
+                const processedDataYaml = yaml.dump(data);
+                fs.writeFileSync(`${directory}/${file}`, processedDataYaml, 
+                    'utf8');
+                const processedDataJson = JSON.stringify(data);
+                fs.writeFileSync(`json/${directory}/${file.split('.')[0]}.json`, 
+                    processedDataJson, 'utf8');
             })
         })
     })
 }
 
-const processReq = (req) => {
+/**
+ * Add max_counted: 1 to each req in the req_list if it is not present
+ * @param {*} req 
+ * @returns req
+ */
+const processReq = (req, filename) => {
     req.forEach((r) => {
         if (!r.hasOwnProperty('max_counted') || r.max_counted === null) 
             r.max_counted = 1;
+        if (r.hasOwnProperty('explanation') && r['explanation'] 
+        && r['explanation'].includes('\n\n')) {
+            console.log("STYLE WARNING -- " + filename + ": " 
+            + "explanation has consecutive newlines in req: " + r.name);
+        }
         if (r.hasOwnProperty('req_list')) 
-            r.req_list = processReq(r.req_list);
+            r.req_list = processReq(r.req_list, filename);
     });
     return req;
 }
 
+/**
+ * Reorder the fields in each req to match the order in the schema
+ * @param {*} req 
+ * @param {string} filename 
+ * @returns req
+ */
 const reorder = (req, filename) => {
-    const FIELDS = ['name', 'min_needed', 'max_counted', 'no_crosslist', 'double_counting_allowed', 'max_common_with_major', 'pdfs_allowed', 'completed_by_semester', 'description', 'explanation', 'req_list', 'course_list', 'excluded_course_list', 'iw_relationship', 'no_req'];
+    const FIELDS = ['name', 'min_needed', 'max_counted', 'no_crosslist', 
+    'double_counting_allowed', 'max_common_with_major', 'pdfs_allowed', 
+    'completed_by_semester', 'explanation', 'req_list', 
+    'course_list', 'excluded_course_list', 'iw_relationship', 'no_req'];
     req.forEach((r) => {
         FIELDS.forEach((f) => {
             if (r.hasOwnProperty(f)) {
-                if ((f === 'iw_relationship' || f === 'min_needed' || f === 'double_counting_allowed' || f === 'explanation' || f === 'description' || f === 'req_list' || f === 'course_list' || f === 'excluded_course_list') && r[f] === null) {
-                    console.log("WARNING -- " + filename + ": " + f + " is null in req: " + r.name);
+                if ((f === 'iw_relationship' || f === 'min_needed' 
+                || f === 'double_counting_allowed' || f === 'explanation' 
+                || f === 'description' || f === 'req_list' 
+                || f === 'course_list' || f === 'excluded_course_list') 
+                && r[f] === null) {
+                    console.log("NULL WARNING -- " + filename + ": " + f 
+                    + " is null in req: " + r.name);
                 }
                 const value = r[f];
                 delete r[f];
@@ -59,4 +106,6 @@ const reorder = (req, filename) => {
     return req;
 }
 
-preprocess();
+const args = process.argv.slice(2);
+if (args.length > 0 && args[0] === 'write') preprocess(true);
+else preprocess(false);
